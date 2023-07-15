581ec19676b97b6b4195b61071f4e42bb612c7f5
victer q <victerqiu@ufl.edu>
2023-07-15 13:24:37 -0400

    this is the beginning of my encode() program

---
+++ main.py
@@ -0,0 +1,12 @@
+# Victer Qiu
+
+def encode(string):
+	encoded_string = []
+	for i in string:
+		x = int(i) + 3
+		if x > 9: # we take the ones digit
+			x %= 10
+		decoded.append(str(x))
+	return ''.join.decoded
+
+print('Menu\n')
