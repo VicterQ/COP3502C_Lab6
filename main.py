1b30066d32a135ff83af1ac8ab50839fe518901b
victer q <victerqiu@ufl.edu>
2023-07-15 14:48:31 -0400

    finished the code and debugged it

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ def encode(string):
 		x = int(i) + 3
 		if x > 9: # we take the ones digit
 			x %= 10
-		decoded.append(str(x))
-	return ''.join.decoded
+		encoded_string.append(str(x))# add x as a string to the list
+	return ''.join(encoded_string) # joinds the list into a string
 
-print('Menu\n')
+while True:
+	print('Menu\n'
+		  '-------------\n'
+		  '1. Encode\n'
+		  '2. Decode\n'
+		  '3. Quit')
+
+	option = int(input('Please enter an option: '))
+	if option == 1:
+		password = input('Please enter your password to encode: ')
+		print('Your password has been encoded and stored!')
+	elif option == 2:
+		print(f'The encoded password is {encode(password)}, and the original password is {password}.')
+	else:
+		break
+
+
