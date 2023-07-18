0657f41a2a67fc39f163e31b4996a5f194af6445
sahilpatel <sahil.patel1@ufl.edu>
2023-07-18 11:53:56 -0400

    Added my decode()

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ def encode(string):
     return encoded_password
 
 
-def decode(decoded_password):
-    pass
+def decode(encoded_password):
+    decoded_password = ""
+    list_password = list(encoded_password)
+    for i in range(len(list_password)):
+        list_password[i] = int(list_password[i]) - 3
+        if list_password[i] < 0:
+            list_password[i] = 10 + list_password[i]
+        decoded_password = decoded_password + str(list_password[i])
     return decoded_password
 
 
@@ -22,14 +28,14 @@ if __name__ == '__main__':
               '-------------\n'
               '1. Encode\n'
               '2. Decode\n'
-              '3. Quit')
+              '3. Quit\n')
         option = int(input('Please enter an option: '))
         if option == 1:
             password = input('Please enter your password to encode: ')
-            print('Your password has been encoded and stored!')
-		    encoded_password = encode(password)
+            print('Your password has been encoded and stored!\n')
+            encoded_password = encode(password)
         elif option == 2:
-			decoded_passwor = decode(encoded_password)
-            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.')
+            decoded_password = decode(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.\n')
         else:
             break
